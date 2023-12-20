65af32078065ede0aa464699c3cbd1b0d8fb81f4
Riyaonweb <126667542+Riyaonweb@users.noreply.github.com>
2023-12-20 22:52:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const megamenu = document.getElementById("mega-menu-box");
+const icon = document.getElementsByTagName("nav")[0].getElementsByTagName("ul")[0].getElementsByTagName("li")[7];
+  
+    function mega() {
+       
+      megamenu.classList.toggle("active");
+icon.classList.toggle("change");
+    }
+
+  
